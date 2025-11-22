cd1a712dfdbc54d0ab3bcfa7c8bc17c2e21c531a
Vikash Mandal <vikasmandal87007@gmail.com>
2025-11-22 20:02:32 +0530

    fix db file path

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
 // src/app/page.jsx
-import HeroSection from "./components/HeroSection";
-import MiddleSection from "./components/MiddleSection";
-import Statistics from "./components/Statistics";
-import FeedbackSlider from "./components/FeedbackSlider";
-import Footer from "./components/Footer";
+import HeroSection from "@/components/HeroSection";
+import MiddleSection from "@/components/MiddleSection";
+import Statistics from "@/components/Statistics";
+import FeedbackSlider from "@/components/FeedbackSlider";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
